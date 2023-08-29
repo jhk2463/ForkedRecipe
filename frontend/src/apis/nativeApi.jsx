d1bd8ef2cca5a0bb6fd0f1e3862ccc216459cf95
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-08-28 21:24:13 -0400

    Updated axios base url for native api

--- frontend/src/apis/nativeApi.jsx
+++ frontend/src/apis/nativeApi.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://forkedrecipebackend.onrender.com/";
 
 const nativeApi = axios.create({
   baseURL: BASE_URL,
