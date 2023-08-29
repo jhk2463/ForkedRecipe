525049b1f5a9847d2724133b12dba3cfad949237
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-08-29 00:19:20 -0400

    Removed console.log statements used for debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./context/AuthProvider";
+// import { AuthProvider } from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
